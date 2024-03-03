dcbdb06cb2b208e3603ff3262c2877bf96aa4798
Eimantas Nėjus <eimantas.nejus@gmail.com>
2024-03-03 21:03:31 +0200

    BB-3: Listing and Creating items

--- films/views.py
+++ films/views.py
@@ -1,10 +1,13 @@
 from django.http.response import HttpResponse
 from django.contrib.auth.views import LoginView
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
+from django.views.generic.list import ListView
 from django.contrib.auth import get_user_model
 
 from films.forms import RegisterForm
+from films.models import Film
 
 
 # Create your views here.
@@ -26,9 +29,33 @@ class RegisterView(FormView):
         return super().form_valid(form)
 
 
+class FilmListView(ListView):
+    template_name = 'films.html'
+    model = Film
+    context_object_name = 'films'
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Film.objects.none()
+        user = self.request.user
+        return user.films.all()
+
+
 def check_username(request):
     """Return Response"""
     username = request.POST.get('username')
     if get_user_model().objects.filter(username=username).exists():
         return HttpResponse('<div id="username-error" class="error">Username already exists</div>', status=200)
     return HttpResponse('<div id="username-error" class="success">Username is available</div>', status=200)
+
+
+def add_film(request):
+    name = request.POST.get('filmname')
+
+    film, is_new = Film.objects.get_or_create(name=name)
+    # add the film to the user's list
+    request.user.films.add(film)
+
+    # return template with all user's films
+    films = request.user.films.all()
+    return render(request, 'partials.film-list.html', {'films': films})
