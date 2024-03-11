f9fb1161db8894f7bffe97ba5abd7c0aeb91dbf7
Eimantas Nėjus <eimantas.nejus@gmail.com>
2024-03-11 20:32:06 +0200

    BB-13: Add search functionality and respect search in result polling requests

--- scores/views.py
+++ scores/views.py
@@ -1,14 +1,22 @@
 import time
 
+from django.db.models import Q
 from django.shortcuts import render
 
 from scores.models import Fixture
 
 
 def fixtures(request):
-    fixtures = Fixture.objects.all()
+    fixtures = Fixture.objects.select_related("home_team", "away_team").all()
 
     is_all_completed = all([fixture.game_finished for fixture in fixtures])
+
+    search = request.GET.get("search")
+    if search:
+        fixtures = fixtures.filter(
+            Q(home_team__name__icontains=search) | Q(away_team__name__icontains=search)
+        )
+
     context = {"fixtures": fixtures, "is_all_completed": is_all_completed}
     if request.htmx:
         time.sleep(1.6)
